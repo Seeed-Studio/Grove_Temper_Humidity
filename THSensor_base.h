553954c635d4c99819e5069fa82124d5e37e3169
zzff-sys <71440760+zzff-sys@users.noreply.github.com>
2021-09-23 16:22:09 +0800

    Update THSensor_base.h

--- THSensor_base.h
+++ THSensor_base.h
@@ -9,15 +9,15 @@ public:
 	uint8_t I2c_Dev_Id;
 	void begin(uint8_t u8Reg);
 	void IIC_WriteCmd(uint8_t u8Cmd);
-    uint16_t IIC_ReadData2byte(uint8_t u8Reg,uint8_t num);
+        uint16_t IIC_ReadData2byte(uint8_t u8Reg,uint8_t num);
 
     
 	uint8_t isAvailable();
-    float ReadTemperature(void);
-    float ReadHumidity(void);
+        float ReadTemperature(void);
+        float ReadHumidity(void);
 	uint16_t IIC_ReadData(void);
 	uint8_t IIC_ReadReg(uint8_t u8Reg,uint8_t num);
-    void IIC_WriteReg(uint8_t u8Reg, uint8_t u8Data);
+        void IIC_WriteReg(uint8_t u8Reg, uint8_t u8Data);
 
 };
 
